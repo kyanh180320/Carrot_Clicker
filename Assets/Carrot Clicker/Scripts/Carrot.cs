2d0e097d2ea1fc9ea96ef7e9e6c405884be7c943
KyAnh <kyanh180320@gmail.com>
2023-08-23 23:46:48 +0700

    Fix bug

--- Assets/Carrot Clicker/Scripts/Carrot.cs
+++ Assets/Carrot Clicker/Scripts/Carrot.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Carrot : MonoBehaviour
 {
     // Start is called before the first frame update
     [Header("Elements")]
     [SerializeField] private Transform carrotRendererTransform;
+
+    [Header("Setting")]
+    [SerializeField] private Image fillImage;
+    [SerializeField] private float fillRate;
     private void Awake()
     {
         InputManager.onCarrotClicked += CarrotClickedCallback;
@@ -27,12 +32,27 @@ public class Carrot : MonoBehaviour
     }
     private void CarrotClickedCallback()
     {
-        print("hello");
+        Animate();
+        Fill();
+       
+       
+       
+    }
+    private void Animate()
+    {
         carrotRendererTransform.localScale = Vector3.one * .8f;
         LeanTween.cancel(carrotRendererTransform.gameObject);
         LeanTween.scale(carrotRendererTransform.gameObject, Vector3.one * .7f, .15f).setLoopPingPong(1);
-       
-       
+    }
+    private void Fill()
+    {
+        fillImage.fillAmount += fillRate;
+        if (fillImage.fillAmount >= 1)
+            StartFenzyMode();
+    }
+    private void StartFenzyMode()
+    {
+        LeanTween.value(1, 0, 5).setOnUpdate((value) => fillImage.fillAmount = value);
     }
 }
 
